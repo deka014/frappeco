47f532436c1ee22066423a3287e8543fd11098c9
Deep <deepjyotideka8@gmail.com>
2021-09-10 20:29:40 +0530

    Refactor app.js

--- app.js
+++ app.js
@@ -1,96 +1,91 @@
-var express   = require("express"),
-app 		  = express(),
-bodyParser    = require("body-parser"), //used in posts
-mongoose 	  = require("mongoose"),
-flash 		  = require("connect-flash"),
-passport 	  = require("passport"),
-LocalStrategy = require("passport-local"),
-methodOverride= require("method-override"),
-faker =  require("faker"),
-session = require("express-session"),
-//data base 
-// Food	 	  	  = require("./models/food"),
-// Comment 	  	  = require("./models/comment"),
-User 		  = require("./models/user"),
-//requiring routes
-commentRoutes	 = require("./routes/comment"),
-foodRoutes	 	 = require("./routes/food"),
-indexRoutes 	 = require("./routes/index");
+//Environment varibales package configuration
+require('dotenv').config()
 
-const MongoStore = require('connect-mongo')(session);
+//Importing node_modules and routes
+const express = require('express'),
+  app = express(),
+  bodyParser = require('body-parser'),
+  mongoose = require('mongoose'),
+  flash = require('connect-flash'),
+  passport = require('passport'),
+  LocalStrategy = require('passport-local'),
+  methodOverride = require('method-override'),
+  faker = require('faker'),
+  session = require('express-session'),
+  User = require('./models/user'),
+  commentRoutes = require('./routes/comment'),
+  foodRoutes = require('./routes/food'),
+  indexRoutes = require('./routes/index'),
+  MongoStore = require('connect-mongo')(session)
 
-mongoose.set("useUnifiedTopology", true); 
-// console.log(process.env.DATABASEURL)  in terminal- export DATABASEURL=mongodb://localhost/yelp_camp
-// var url = process.env.DATABASEURL || "mongodb://localhost/food_blog"
-// mongoose.connect("mongodb://localhost/fblog",{ useNewUrlParser: true });
-mongoose.connect("mongodb+srv://rondeka:hello@123@frappeco.k1hqv.mongodb.net/frappeco?retryWrites=true&w=majority", {useCreateIndex: true,useNewUrlParser: true });
+mongoose.set('useUnifiedTopology', true)
 
-//  async function seedPosts() {
-//  	await Food.remove({});
-//  	for(const i of new Array(30)) {
-//  			const post = {		
-//  				title: faker.lorem.word(),
-// 				image1: faker.image.image(),
-// 				category: 'travel',
-//  				description: faker.lorem.text(),
-//  				author: {
-//  			    '_id' : '5bb27cd1f986d278582aa58c',
-//  			    'username' : 'ron'
-//  				}
-//  			}
-//  			await Food.create(post);
-//  	}
-//  	console.log('40 new posts created');
-//  }
+//Mongoose connection setup
+mongoose
+  .connect(process.env.MONGO_DB_URI, {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+  })
+  .then(() => console.log('Database Connected'))
+  .catch((err) => console.log(err))
 
-// seedPosts();
+app.use(bodyParser.urlencoded({ extended: true }))
+app.set('view engine', 'ejs')
+app.use(express.static(__dirname + '/public'))
+app.use(methodOverride('_method'))
+app.use(flash())
 
-app.use(bodyParser.urlencoded({extended: true}));
-app.set("view engine" , "ejs");
-app.use(express.static(__dirname+"/public"));  //dir name is yelp camp
-app.use(methodOverride("_method"));
-app.use(flash());
+//Passsport configuration for authentication
+app.use(
+  session({
+    secret: process.env.PASSPORT_SESSION_SECRET,
+    // resave				: false,
+    // saveUninitialized 	: false
+    cookie: { maxAge: 60000 * 60 * 24 * 7 }, //in milliseconds.
+    resave: false, //Don't save the session to store if it hasn't changed
+    rolling: true, //Reset the cookie Max-Age on every request
+    saveUninitialized: false, //Don't create a session for anonymous users
+    secret: process.env.PASSPORT_SESSION_SECRET,
+    store: new MongoStore({ mongooseConnection: mongoose.connection }),
+  })
+)
 
-//passsport configuration
-app.use(session({
-	secret  			: "youtube vs tiktok",
-	// resave				: false,
-	// saveUninitialized 	: false
-		cookie: { maxAge : 60000 * 60 * 24 * 7  },     //in milliseconds.
-        resave: false,                  //Don't save the session to store if it hasn't changed
-        rolling: true,                  //Reset the cookie Max-Age on every request
-        saveUninitialized: false,       //Don't create a session for anonymous users
-        secret: "youtube vs tiktok",
-        store: new MongoStore({ mongooseConnection: mongoose.connection })
-}))
+app.use(passport.initialize())
+app.use(passport.session())
+passport.use(
+  new LocalStrategy(
+    {
+      usernameField: 'email',
+      usernameQueryFields: ['email'],
+    },
+    User.authenticate()
+  )
+)
+passport.serializeUser(User.serializeUser())
+passport.deserializeUser(User.deserializeUser())
 
-app.use(passport.initialize());
-app.use(passport.session());
-// passport.use(new LocalStrategy(User.authenticate()));//user.authenticate is from passport package
-passport.use(new LocalStrategy ({
-  usernameField: 'email',
-  usernameQueryFields: ['email']
-},User.authenticate()));
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+app.use(function (req, res, next) {
+  // now  you dont need to them to the ejs separetly
+  res.locals.currentUser = req.user
+  res.locals.error = req.flash('error')
+  res.locals.message = req.flash('success')
+  ;(res.locals.title = 'Frappeco - Never go empty'),
+    (res.locals.meta = {
+      description:
+        'Frappeco is a platform to everyone, starting from gourmets and connoisseur to bibliophiles to share their relishing memories of food.',
+      robot: 'index, follow, max-snippet:-1, max-image-preview:large ',
+    })
+  res.locals.paginatedUrl =
+    req.originalUrl.replace(/(\?|\&)page=\d+/g, '') + '?'
+  next()
+})
 
-app.use(function(req,res,next){
-	res.locals.currentUser = req.user;// now  i dont need to {add currentUser : req.user} in every step
-	res.locals.error = req.flash("error");
-	res.locals.message = req.flash("success");
-	res.locals.title = "Frappeco - Never go empty", 
-	res.locals.meta = {description: "Frappeco is a platform to everyone, starting from gourmets and connoisseur to bibliophiles to share their relishing memories of food.", robot : "index, follow, max-snippet:-1, max-image-preview:large "};
-	res.locals.paginatedUrl = req.originalUrl.replace(/(\?|\&)page=\d+/g, '') + "?"
-	next();
-});
+//Using the route files
 
-//reqquires from the route folder
-app.use(indexRoutes);
-app.use(foodRoutes);  ///campgrounds means all the routes will be starting from campgrounds/xyz
-app.use("/:id/:id1/comments",commentRoutes);
+app.use(indexRoutes)
+app.use(foodRoutes)
+app.use('/:id/:id1/comments', commentRoutes)
 
-
-app.listen(process.env.PORT || 5000, function() {
-    console.log("Server started");
-  }); 
-	
+app.listen(process.env.PORT || 5000, function () {
+  console.log('Server started')
+})
